c9f193a02244c87e45f020273ac073cd400e99ee
Andrea Baschiera <andrea.blusurf@gmail.com>
2026-08-05 00:06:59 +0200

    Refactored datamodel in ts and import from taxonomy.json instead of hard-coded example

--- src/taskpane/datamodel.ts
+++ src/taskpane/datamodel.ts
@@ -1,178 +1,53 @@
-export type TagType =
-  | "textBlock"
-  | "string"
-  | "boolean"
+export type Datatype =
   | "enumeration"
-  | "enumerationSet"
+  | "narrative"
+  | "boolean"
+  | "enumerationset"
+  | "percent"
+  | "monetary"
   | "date"
+  | "string"
+  | "undefined"
   | "year"
-  | "numeric"
+  | "ghgemissions"
   | "decimal"
-  | "percentage";
+  | "energy"
+  | "mass"
+  | "volume"
+  | "integer";
 
 export interface Taxonomy {
-  tags: TaxonomyTag[];
+  datapoints: Datapoint[];
 }
 
-export interface TaxonomyTag {
-  id: string;
+export interface Datapoint {
+  reference: string;
+  dr: string;
+  datatype: Datatype;
   name: string;
-  type: TagType;
-  values?: TaxonomyValue[];
-  dimension?: Dimension;
+  qname: string;
+  enum?: Enumeration;
+  dimensions?: Dimension[];
 }
 
-export interface TaxonomyValue {
-  id: string;
+export interface ValueLabel {
+  value: string;
   label: string;
 }
 
+export interface Enumeration {
+  enum_id: string;
+  enum_members: ValueLabel[];
+}
+
 export interface Dimension {
-  axis: string;
-  mode: "explicit" | "typed";
-  members?: DimensionMember[];
+  required: boolean;
+  dimension_id: string;
+  dimension_mode: "fixed" | "typed";
+  dimension_title: string;
+  dimension_members?: ValueLabel[];
 }
 
-export interface DimensionMember {
-  memberId: string;
-  memberLabel: string;
-}
+import data from "../python/taxonomy.json";
 
-export const taxonomy: Taxonomy = {
-  tags: [
-    {
-      id: "text_simple",
-      name: "Datapoint representing a textblock",
-      type: "textBlock"
-    },
-
-    {
-      id: "string_simple",
-      name: "Datapoint representing a string",
-      type: "string",
-    },
-
-    {
-      id: "boolean_simple",
-      name: "Datapoint representing a boolean",
-      type: "boolean"
-    },
-
-    {
-      id: "enum_short",
-      name: "Datapoint representing a short enumeration",
-      type: "enumeration",
-      values: [
-        { id: "LOW", label: "Low" },
-        { id: "MEDIUM", label: "Medium" },
-        { id: "HIGH", label: "High" }
-      ]
-    },
-
-    {
-      id: "enum_long",
-      name: "Datapoint representing a long enumeration",
-      type: "enumeration",
-      values: [
-        { id: "LOW", label: "Low" },
-        { id: "MEDIUM", label: "Medium" },
-        { id: "HIGH", label: "High" },
-        { id: "SUPERHIGH", label: "Super High" },
-        { id: "SUPERLOW", label: "Super Low" }
-      ]
-    },
-
-    {
-      id: "enum_set",
-      name: "Datapoint representing an enumeration Set",
-      type: "enumerationSet",
-      values: [
-        { id: "DE", label: "Germany" },
-        { id: "IT", label: "Italy" },
-        { id: "FR", label: "France" },
-        { id: "PT", label: "Portugal" },
-        { id: "ES", label: "Spain" }
-      ]
-    },
-
-    {
-      id: "number_disag-expl",
-      name: "Datapoint representing number disaggregated by fixed dim members",
-      type: "numeric",
-      dimension: {
-        axis: "Geography",
-        mode: "explicit",
-        members: [
-          { memberId: "BE", memberLabel: "Belgium" },
-          { memberId: "FR", memberLabel: "France" },
-          { memberId: "DE", memberLabel: "Germany" }
-        ]
-      }
-    },
-
-    {
-      id: "number_disag-typed",
-      name: "Datapoint representing number disaggregated by typed dim",
-      type: "numeric",
-      dimension: {
-        axis: "Department",
-        mode: "typed"
-      }
-    },
-
-    {
-      id: "enum-short_disag-expl",
-      name: "Datapoint representing a short enumeration disaggregated by fixed dim members",
-      type: "enumeration",
-      values: [
-        { id: "LOW", label: "Low" },
-        { id: "HIGH", label: "High" }
-      ],
-      dimension: {
-        axis: "Geography",
-        mode: "explicit",
-        members: [
-          { memberId: "BE", memberLabel: "Belgium" },
-          { memberId: "DE", memberLabel: "Germany" }
-        ]
-      }
-    },
-
-    {
-      id: "enum-set_disag-expl",
-      name: "Datapoint representing an enumeration Set disaggregated by fixed dim members",
-      type: "enumerationSet",
-      values: [
-        { id: "LOW", label: "Low" },
-        { id: "HIGH", label: "High" },
-        { id: "HIGHDRY", label: "High and dry" },
-        { id: "SUPERHIGH", label: "Super High" },
-        { id: "SUPERLOW", label: "Super Low" }
-      ],
-      dimension: {
-        axis: "Geography",
-        mode: "explicit",
-        members: [
-          { memberId: "BE", memberLabel: "Belgium" },
-          { memberId: "DE", memberLabel: "Germany" },
-          { memberId: "IT", memberLabel: "Italy" },
-          { memberId: "CI", memberLabel: "something else" },
-        ]
-      }
-    },
-
-    {
-      id: "text_disag-expl",
-      name: "Datapoint representing a text disaggregated by fixed dim members",
-      type: "textBlock",
-      dimension: {
-        axis: "Business unit",
-        mode: "explicit",
-        members: [
-          { memberId: "HR", memberLabel: "Human Resources" },
-          { memberId: "IT", memberLabel: "IT" }
-        ]
-      }
-    }
-  ]
-};
+export const taxonomy: Taxonomy = {datapoints: data as Datapoint[]};
